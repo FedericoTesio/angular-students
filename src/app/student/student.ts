6eab667dd119b1faa5510f21f2e01c1337c6a117
unknown <f.tesio.3619@vallauri.net>
2026-09-21 10:35:04 +0200

    Sintassi alternativa per ngStyle

--- src/app/student/student.ts
+++ src/app/student/student.ts
@@ -9,12 +9,20 @@ import { CommonModule } from '@angular/common';
 })
 export class Student {
 
-  M_COLOR = 'lightblue'
-  F_COLOR = 'pink'
+  M_COLOR = "lightblue";
+  F_COLOR = "pink";
 
   students:any[] = [{"name": "Bumbuz", "city": "Saluzzo", "gender": "M", "present": true}, 
                     {"name": "Tesio", "city": "Bagge", "gender": "M", "present": false},
                     {"name": "Mossello", "city": "Monasterolo di Savigliano", "gender": "M", "present": true},
                     {"name": "Parola", "city": "Centallo", "gender": "F", "present": true}
   ];
+
+  getStyle(s: any){
+    return{
+      'backgroundColor': s.gender == 'F' ? this.F_COLOR : this.M_COLOR,
+      'textDecoration': s.present ? 'none' : 'underline',
+      'font-weight': s.present ? 'normal' : 'bold'
+    }
+  }
 }
